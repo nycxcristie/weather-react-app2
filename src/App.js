6f5a11fdf47dbe0b3b5caf7b49f04acecb7b6402
nycxcristie <104367483+nycxcristie@users.noreply.github.com>
2023-06-07 18:23:59 -0400

    updated loader color from yellow to blue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           height="80"
           width="80"
           radius={1}
-          color="yellow"
+          color="blue"
           ariaLabel="puff-loading"
           wrapperStyle={{}}
           wrapperClass=""
